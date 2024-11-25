42148da18467cbe8caa95afffb27978b2b09979a
Joseph L. Lane <lanejl@lincolncountyne.gov>
2024-11-25 17:08:10 -0600

    Lots of stuff

--- cashdrawer.rb
+++ cashdrawer.rb
@@ -1,19 +1,42 @@
 require 'glimmer-dsl-libui'
 require 'time'
+require 'money'
 
 class CashDrawer
 
 	include Glimmer
-
-	Funds = Struct.new(
+	
+	Money.locale_backend = :currency
+	Money.default_currency = 'USD'
+	Money.rounding_mode = BigDecimal::ROUND_HALF_UP
+	
+	Drawer = Struct.new(
+		:penny,
+		:nickel,
+		:dime,
+		:quarter,
 		:ones,
 		:fives,
 		:tens,
 		:twentys,
 		:fiftys,
-		:bennys,
+		:bennys
+	)
+	
+	Available = Struct.new(
 		:checks,
-		:petty
+		:bank,
+		:mvdue,
+		:petty,
+		:cbag
+	)
+	
+	Payable = Struct.new(
+		:office,
+		:copy,
+		:motors,
+		:guns,
+		:prints
 	)
 	
 	Total = Struct.new( :date, :dtotal, :onhand, :ap )
@@ -22,7 +45,9 @@ class CashDrawer
 	attr_accessor :ones, :fives, :tens, :twentys, :fiftys, :bennys
 	attr_accessor :penny, :nickel, :dime, :quarter
 	attr_accessor :checks, :petty, :bank, :mvdue, :cbag
+	attr_accessor :office, :copy, :motors, :guns, :prints
 	attr_accessor :date, :dtotal, :onhand, :ap, :dt
+	
 
 	def initialize
 		@totals = [
@@ -73,6 +98,7 @@ class CashDrawer
 	
 		window('Cash Drawer', 400, 300, has_menubar = true) {
 			margined true
+			
 			tab {
 				tab_item('Funds') {
 				
@@ -80,7 +106,7 @@ class CashDrawer
 						horizontal_box {
 						
 							group {
-							stretchy false
+								stretchy false
 								vertical_box {
 									form {
 										stretchy false
@@ -88,29 +114,33 @@ class CashDrawer
 										date_picker {
 											label 'Date'
 											time <=> [self, :date]
-											@date = "#{Time.now.year}-#{Time.now.month}-#{Time.now.day}"
+											@date = "#{Time.now.year}-#{'%02d' % Time.now.month}-#{'%02d' % Time.now.day}"
 											on_changed do
-												@date = "#{date[:year]}-#{date[:mon]}-#{date[:mday]}"
+												@date = "#{date[:year]}-#{'%02d' % date[:mon]}-#{'%02d' % date[:mday]}"
 											end
 										}
 										
 										
 										entry {
+											@penny = '0'
 											label 'Pennies'
 											text <=> [self, :penny]
 										}
 										
 										entry {
+											@nickel = '0'
 											label 'Nickels'
 											text <=> [self, :nickel]
 										}
 										
 										entry {
+											@dime = '0'
 											label 'Dimes'
 											text <=> [self, :dime]
 										}
 										
 										entry {
+											@quarter = '0'
 											label 'Quarters'
 											text <=> [self, :quarter]
 										}
@@ -119,37 +149,43 @@ class CashDrawer
 							}
 							
 							group{
-							stretchy false
+								stretchy false
 								vertical_box {
 									form {
 										stretchy false
 										
 										entry {
+											@ones = '0'
 											label 'Ones'
 											text <=> [self, :ones]
 										}
 										
 										entry {
+											@fives = '0'
 											label 'Fives'
 											text <=> [self, :fives]
 										}
 										
 										entry {
+											@tens = '0'
 											label 'Tens'
 											text <=> [self, :tens]
 										}
 										
 										entry {
+											@twentys = '0'
 											label 'Twentys'
 											text <=> [self, :twentys]
 										}
 										
 										entry {
+											@fiftys = '0'
 											label 'Fiftys'
 											text <=> [self, :fiftys]
 										}
 										
 										entry {
+											@bennys = '0'
 											label 'Hundreds'
 											text <=> [self, :bennys]
 										}
@@ -158,47 +194,149 @@ class CashDrawer
 							}
 
 							group {
-							stretchy false
+								stretchy false
 								vertical_box {
 									form {
 										stretchy false
 										
 										entry {
+											@checks = '0'
 											label 'Checks'
 											text <=> [self, :checks]
 										}
 										
 										entry {
+											@bank = '0'
+											label 'Bank'
+											text <=> [self, :bank]
+										}
+										
+										entry {
+											@mvdue = '0'
+											label 'MV Due'
+											text <=> [self, :mvdue]
+										}
+										
+										entry {
+											@petty = '0'
 											label 'Petty Cash'
 											text <=> [self, :petty]
 										}
 										
 										entry {
-											label 'On Hand'
-											text <=> [self, :onhand]
+											@cbag = '0'
+											label 'Cash Bag'
+											text <=> [self, :cbag]
 										}
 									}
-								
 								}
 							}
 						}
-						
-						group {
-						stretchy false
+						horizontal_box {
+							stretchy false
+							
+							group {
+								stretchy false
 							vertical_box {
 								form {
 									stretchy false
-									button('Submit') {
-										stretchy false
-										
-										on_clicked do
-											@totals << Total.new(@date, @dtotal, @onhand, @ap)
-											msg_box('Notice', 'Values have been added to the ledger.')
-										end
+									
+									entry {
+										@office = '0'
+										label 'Office'
+										text <=> [self, :office]
+									}
+									
+									entry {
+										@copy = '0'
+										label 'Copy'
+										text <=> [self, :copy]
+									}
+									
+									entry {
+										@motors = '0'
+										label 'Motors'
+										text <=> [self, :motors]
+									}
+									
+									entry {
+										@guns = '0'
+										label 'Firearms'
+										text <=> [self, :guns]
+									}
+									
+									entry {
+										@prints = '0'
+										label 'Prints'
+										text <=> [self, :prints]
 									}
 								}
 							}
+							}
+							
+							vertical_box {
+								form {
+									stretchy false
+								}
+							}
+							
+							
+							
+							vertical_box {
+								form {
+									stretchy false
+								}
+							
+							}
 						}
+						
+						#group {
+						#	stretchy false
+							
+						#	vertical_box {
+								
+							
+								horizontal_box {
+									#form {
+										stretchy false
+										
+										button('Calculate') {
+											stretchy false
+											
+											on_clicked do
+												@dtotal = BigDecimal(@penny) + BigDecimal(@nickel) + 
+													BigDecimal(@dime) + BigDecimal(@quarter) +
+													BigDecimal(@ones) + BigDecimal(@fives) + 
+													BigDecimal(@tens) + BigDecimal(@twentys) + 
+													BigDecimal(@fiftys) + BigDecimal(@bennys)
+													
+												@onhand = BigDecimal(@checks) + BigDecimal(@bank) +
+													BigDecimal(@mvdue) + BigDecimal(@petty) + BigDecimal(@cbag)
+													
+												@ap = 
+												
+												p Money.from_amount(@onhand).format
+												p Money.from_amount(@dtotal).format
+												p Money.from_amount(@ap).format
+											end
+										}
+										
+										button('Submit') {
+											stretchy false
+											
+											on_clicked do
+												if @onhand.nil? then @onhand = "0.00" end
+												if @dtotal.nil? then @dtotal = "0.00" end
+												if @ap.nil? then @ap = "0.00" end
+												
+												@totals << Total.new(@date, Money.from_amount(@dtotal).format, Money.from_amount(@onhand).format, @ap)
+												msg_box('Notice', 'Values have been added to the ledger.')
+											end
+										}
+									#}
+								}
+							#}
+						#}
 					}
 				}
 				
